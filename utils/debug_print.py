dade09180a2ee6d47d007fcef3d825b23a4df96a
GuyYar1 <132554415+GuyYar1@users.noreply.github.com>
2024-06-16 15:53:27 +0300

    minor

--- utils/debug_print.py
+++ utils/debug_print.py
@@ -3,7 +3,7 @@ import os
 
 
 def debug_print(text):
-    # pYCHARM ENV VVARIABLE DOESNT  WORK." SET IT ON SUSER ENV VAR AND JUST CHANGE IT 
-    debug_mode = FALSE # os.getenv('DEBUG_MODE') == 'true'
+    # pYCHARM ENV VVARIABLE DOESNT  WORK." SET IT ON SUSER ENV VAR AND JUST CHANGE IT
+    debug_mode = 'false'  # os.getenv('DEBUG_MODE') == 'true'
     if debug_mode:
         print(text)
